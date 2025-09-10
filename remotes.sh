33a90f74bf741eb7acf784f666138fa2e69a281b
Elidiane <121459020+elidianesampaiotrabuco@users.noreply.github.com>
2025-09-10 10:47:30 -0300

    Update remotes.sh
    gitlab support not implemented yet

--- remotes.sh
+++ remotes.sh
@@ -1,5 +1,5 @@
 git remote remove origin
-git remote add github https://github.com/p01arst0rm/reactos-modern/
+git remote add github https://github.com/elidianesampaiotrabuco/HoloV2/
 git remote add gitlab https://gitlab.com/p01arst0rm/reactos-modern.git
 
 
